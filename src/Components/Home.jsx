3cb42a6bac5ef015170266b5115985f45f944ecc
Saloni Suman <salonisuman06@gmail.com>
2023-10-28 17:26:33 +0530

    changed files

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "..\images\student-working.jpg";
+import image from "../images/student-working.jpg";
 
 const imageAltText = "Adult female in office working on a laptop with a coffee mug kept at table";
 
